5ce7a23ea335c47dacc75ed888dc9548dbbf0984
Deleciar <ramsahaishayen@gmail.com>
2026-04-26 15:44:17 +0100

    Add resume site from shayenr.github.io

---
+++ src/content_option.js
@@ -0,0 +1,220 @@
+const logotext = "SHAYEN";
+const meta = {
+    title: "Shayen Ramsahai",
+    description: "I am Shayen Ramsahai _ Engineer _ PhD cadidate in Blockchain adoption _ Currently residing in the Isle of Man",
+};
+
+const introdata = {
+    title: "Shayen Ramsahai",
+    animated: {
+        first: "Web3.0 researcher",
+        second: "Crypto currency and Blockchain specialist",
+        third: "Technology enthusiast",
+    },
+    description: "",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "",
+};
+const qualifications = [{
+        degree: "Bachelor of Technology: Electronic Engineering",
+        university: "Durban University of Technology (DUT)",
+        country: "South Africa"
+    },
+    {
+        degree: "Bachelor of Technology: Management",
+        university: "Durban University of Technology (DUT)",
+        country: "South Africa"
+    },
+    {
+        degree: "Master of Science: Blockchain and Digital Currencies",
+        university: "University of Nicosia (UNIC)",
+        country: "Cyprus"
+    },
+    {
+        degree: "PhD in Business Administration: Blockchain adoption - in progress",
+        university: "University of Nicosia (UNIC)",
+        country: "Cyprus"
+    },
+];
+const worktimeline = [{
+        jobtitle: "IT Engineer",
+        where: "Derivco - Isle of Man",
+        date: "2019-present",
+    },
+    {
+        jobtitle: "Software Quality Engineer",
+        where: "Derivco - South Africa",
+        date: "2016-2019",
+    },
+    {
+        jobtitle: "Quality Assurance",
+        where: "Derivco - South Africa",
+        date: "2013-2016",
+    },
+];
+
+const skills = [{
+        name: "Firewall provisioning",
+        value: 80,
+    },
+    {
+        name: "Server provisioning",
+        value: 85,
+    },
+    {
+        name: "SQL admin",
+        value: 80,
+    },
+    {
+        name: "MS Azure DevOps",
+        value: 80,
+    },
+    {
+        name: "F5 loadbalancing",
+        value: 80,
+    },
+     {
+        name: "Monitoring and observability",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Casino enterprise solutions",
+        description: "",
+    },
+    {
+        title: "Blockchain design",
+        description: "",
+    },
+    {
+        title: "Crypto currency design",
+        description: "",
+    },
+    {
+        title: "Research methodologies",
+        description: "",
+    },
+     {
+        title: "Process optimisation",
+        description: "",
+    },
+    {
+        title: "Strategic planning",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/id/48/400/?grayscale",
+        description: "Casino enterprise software",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Blockchain conceptual architectural designs",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Observability and management",
+        link: "#",
+    },
+    {
+         img: "https://picsum.photos/400/800/?grayscale",
+        description: "Backend infrastructure",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Leadership",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/180/400/?grayscale",
+        description: "Research",
+        link: "#",
+    },
+
+    {
+         img: "https://picsum.photos/400/700/?grayscale",
+        description: "Crypto currency trading",
+    
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "DevOps",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/366/400/550/?grayscale",
+        description: "Technology",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Casino backoffice management",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Crypto currency design",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Quality",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/318/400/?grayscale",
+        description: "Travel",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Health",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shayen.ramsahai@gmail.com",
+    YOUR_FONE: "+447624290895",
+    description: "Feel free to email me or contact me on whatsapp for any queries",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    qualifications,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
